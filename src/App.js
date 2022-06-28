dbef8ce9626d1e489646fa84b803094af359f6b6
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-06-28 17:28:40 -0400

    ReactJS lesson: source file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
+import UseContextSection from './components/UseContextSection';
+import UseEffectSection from './components/UseEffectSection';
+import UseStateSection from './components/UseStateSection';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container my-5 mx-auto">
+        <UseStateSection/>
+          <hr />
+        <UseEffectSection/>
+          <hr />
+        <UseContextSection/>
+      </div>
     </div>
   );
 }
